7bd90fad70366c24491d1ace527f83c1d91b477a
DemetrioPadre <demetriopadre@gmail.com>
2024-02-26 15:12:40 +0100

    add table

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+include_once ('./result.php')
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-hotel</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <table class="table">
+        <thead>
+            <tr>
+                <th class="scope">Name</th>
+                <th class="scope">Description</th>
+                <th class="scope">Parking</th>
+                <th class="scope">Vote</th>
+                <th class="scope">Distance to Center</th>
+            </tr>
+        </thead>
+
+    </table>
+
+</body>
+
+</html>
